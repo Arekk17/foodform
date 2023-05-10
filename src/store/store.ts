ed9fa7986476fddf8984bdbab2d7a292235d845f
Arek Daszkowski <daszkowskiarek145@gmail.com>
2023-05-10 20:43:47 +0200

    creating a redux store file and delete the comm

---
+++ src/store/store.ts
@@ -0,0 +1,12 @@
+// store.ts
+
+import { createStore, combineReducers } from 'redux';
+import { reducer as formReducer } from 'redux-form';
+
+const rootReducer = combineReducers({
+  form: formReducer,
+});
+
+const store = createStore(rootReducer);
+
+export default store;
