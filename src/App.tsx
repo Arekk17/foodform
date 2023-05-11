75cc7878a28c92f7d490cbc149c8e7ebb8e88a15
Arek Daszkowski <daszkowskiarek145@gmail.com>
2023-05-11 14:49:23 +0200

    adding styles to index.tsx changing the name's components

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,14 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import store from './store/store';
-import MyForm from './components/DishForm/DishForm';
+import DishForm from './components/DishForm/DishForm';
 import './App.css'
 
 const App: React.FC = () => {
   return (
     <Provider store={store}>
       <div className='form'>
-        <MyForm />
+        <DishForm />
       </div>
     </Provider>
   );
